763850275a12a35213fd0ddc383b73421bd70c75
necoder27 <laetitiaelbadry@gmail.com>
2025-04-21 10:16:02 +0200

    scaffolding react app with shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+import { Header } from "@/components/header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h2>Listen to anything, anywhere</h2>
+      <h3>Transform text, websites, and documents into natural-sounding speech</h3>
+      <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button.tsx";
+
+function Header() {
+  return (
+    <div>
+      <h1>yapit</h1>
+      <Button>h</Button>
+    </div>
+  )
+}
+
+export { Header }
