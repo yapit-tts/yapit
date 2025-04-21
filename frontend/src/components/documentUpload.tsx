f835f08abfb256656a3ed690232b2ccb0eaaec8f
necoder27 <laetitiaelbadry@gmail.com>
2025-04-21 22:03:32 +0200

    feat: improve document upload component

--- frontend/src/components/documentUpload.tsx
+++ frontend/src/components/documentUpload.tsx
@@ -1,9 +1,14 @@
 import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Play } from "lucide-react";
 
 function DocumentUpload() {
 	return (
-		<div>
-			<Textarea />
+		<div className="flex flex-col w-full items-center space-y-8">
+			<div className="flex flex-row w-[50%]">
+				<Textarea />
+			</div>
+			<Button><Play />&nbsp;Start&nbsp;Listening</Button>
 		</div>
 	)
 }
