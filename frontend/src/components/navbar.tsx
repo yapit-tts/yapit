8e3d5f6a26a8454ec83c21e2d8158a1a710a5e7a
necoder27 <laetitiaelbadry@gmail.com>
2025-04-24 14:23:43 +0200

    feat: add basic playback page

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button.tsx";
 import { Settings } from "lucide-react";
 
-function Navbar() {
+const Navbar = () => {
   return (
     <div className="flex flex-row fixed right-0 top-0 h-fit m-2 p-3 rounded-xl backdrop-blur-lg space-x-6">
       <h1 className="text-3xl">yapit</h1>
--- frontend/src/pages/PlaybackPage.tsx
+++ frontend/src/pages/PlaybackPage.tsx
@@ -1,8 +1,10 @@
 import { SoundControl } from '@/components/soundControl';
+import { DocumentCard } from '../components/documentCard';
 
 const PlaybackPage = () => {
 	return (
 		<div>
+			<DocumentCard />
 			<SoundControl />
 		</div>
 	)
