8e9f856828a68f11c1788fb2a51ad6e4c82bb77d
lukasl-dev <git@lukasl.dev>
2025-06-06 15:12:52 +0200

    feat(auth): add ApiProvider to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { StackProvider } from "@stackframe/react";
 import "./App.css";
 import AppRoutes from "@/routes/AppRoutes";
 import { stackClientApp } from "@/auth";
+import { ApiProvider } from "@/api";
 
 function App() {
 	return (
 		<>
 			<StackProvider app={stackClientApp}>
-				<AppRoutes />
+				<ApiProvider>
+					<AppRoutes />
+				</ApiProvider>
 			</StackProvider>
 		</>
 	);
